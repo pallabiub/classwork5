8c2dbbec85d6e2493dd7cee6f375e972711a384e
pallab biswas <bpallab63@gmail.com>
2018-07-25 05:18:04 +0000

    classwork 8

--- server.js
+++ server.js
@@ -4,6 +4,26 @@
   var app = express();
   var server = http.Server(app);
   
+  var mongo =require('mongodb');
+  var db, uri= "mongodb://"+process.env.IP+":28017";
+   mongo.MongoClient.connect(uri,
+   {useNewUrlParser:true},
+   function(err, client){
+     if(err){
+       console.log('could not connect to mongodb')
+      } else{
+         db=client.db('node-cw8');
+       }
+     }
+   )
+  
+  
+  var save =function(form_data){
+    db.createCollection('users',function(err, collection){});
+    var collection =db.collection('users');
+    collection.save(form_data);
+  }
+  
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   
@@ -15,9 +35,11 @@
   });
   app.get('/form', function(req, res){
     res.sendFile(__dirname+'/form.html');
+    save(req.body);
   });
   app.post('/submit_user', function(req, res){
     console.log(JSON.stringify(req.body));
+    save(req.body);
   });
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
